860cb9b31ef72505c51ac579c5ff7f0e0c9bee32
trantronghieu222 <tranhieu4345@gmail.com>
2024-02-23 12:15:58 +0700

    update js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
 /* Thêm class darkMode vào body */
 function handleDarkNight() {
-    document.getElementById("myBody").classList.toggle("darkMode");
+  document.getElementById("myBody").classList.toggle("darkMode");
+}
+/* Cuộn Header */
+window.onscroll = function () { myFunction() };
+var header = document.getElementById("header__main");
+var sticky = header.offsetTop;
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("nav-fixed");
+  } else {
+    header.classList.remove("nav-fixed");
   }
-  /* Cuộn Header */
-  window.onscroll = function() {myFunction()};
-  var header = document.getElementById("header__main");
-  var sticky = header.offsetTop;
-  function myFunction() {
-    if (window.pageYOffset > sticky) {
-      header.classList.add("nav-fixed");
-    } else {
-      header.classList.remove("nav-fixed");
-    }
-  }
+}
